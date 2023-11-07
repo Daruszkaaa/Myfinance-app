b0b34ec0e385f92af813b487f8edd733b1efcaca
Daria Błaszczak-Brzegowy <daria.blaszczakk@gmail.com>
2023-11-07 22:53:52 +0100

    styled SignIn view

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router-dom';
+import { Password } from 'primereact/password';
+import photo from '../assets/AdobeStock_509395296.jpeg'
 
+export default function Signin() {
 
-export default function SignIn(){
+    const navigate = useNavigate();
 
-    return(
-        <>
-        Test
-        </>
-    )
-}
+        return (
+            <section className="signin">
+                <div className='signin-menu'>
+                    <h1 className= "logo"><span>My</span>Finances</h1>
+                </div>
+                <div className="signin-container">
+                    <img src={photo}/>
+
+                    <div className="signin-form-container">
+                        <h2>LOGOWANIE</h2>
+                    <br/>
+                    <form className='signin-form'>
+                <span className="p-input-icon-right">
+                    <InputText placeholder="Email"/>
+                </span>
+                        <br/>
+                        <span className="p-input-icon-left">
+                  <Password placeholder="Password" toggleMask feedback={false}/>
+                    </span>
+                        <br/>
+                        <br />
+                        <Button type={"submit"}  label="Zaloguj" />
+                        <br />
+                        <p>Nie masz jeszcze konta?</p>
+                        <Button onClick={() => navigate('/signup')} label="Zarejestruj się" severity="info" text raised />
+                    </form>
+                </div>
+            </div>
+            </section>
+        )
+    }
