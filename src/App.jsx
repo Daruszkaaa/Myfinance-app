53c281b46f281e127569ee5a1098f2fc401a973e
Daria Błaszczak-Brzegowy <daria.blaszczakk@gmail.com>
2023-11-07 21:47:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createRoot} from "react-dom/client";
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+
+//VIEWS
+import Finance from "./views/Finance.jsx";
+import SignIn from './views/SignIn.jsx';
+import SignUp from "./views/SignUp.jsx";
+
+function App() {
+  return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<SignIn />} />
+                <Route path='/signin' element={<SignIn />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/finance' element={<Finance />} />
+            </Routes>
+        </Router>
+  )
+}
+
+export default App
